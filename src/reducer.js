d7202323be9acfb39e24cbb73d049be154330f46
Ashish Tiwari <ashishtiwari05032002@gmail.com>
2021-12-28 14:17:04 +0530

    make it better

---
+++ src/reducer.js
@@ -0,0 +1,31 @@
+export const initialState = {
+    user: null,
+    roomName: null,
+    contact:null,
+    lastseen:null
+};
+
+export const actionTypes = {
+    SET_USER: "SET_USER",
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case actionTypes.SET_USER:
+            return {
+                ...state,
+                user: action.user,
+            };
+        case actionTypes.SET_ROOM:
+            return {
+                ...state,
+                roomName: action.room,
+                contact:action.contact,
+                lastseen:action.lastseen
+            };
+        default:
+            return state;
+    }
+};
+
+export default reducer;
